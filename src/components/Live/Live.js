43a0262ba52394d2bd76ed03b8628ebf307f79b6
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-04 00:50:14 +0100

    using useParams to retrieve slug

--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
+import {useParams} from 'react-router-dom';
 
 function Live() {
-
+    let {slug} = useParams();
     return (
         <div>
-            <ReactTwitchEmbedVideo height="754" width="100%" channel="boxbox">
+            <ReactTwitchEmbedVideo height="754" width="100%" channel={slug}>
 
             </ReactTwitchEmbedVideo>
         </div>
