1dda3f94ad91b852e62be78169ad0ff87e97571e
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-03 22:23:28 +0100

    Création des cartes de la page d'accueil

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -7,15 +7,32 @@ function Games() {
     useEffect(()=>{
         const fetchData = async  () => {
             const res = await api.get('https://api.twitch.tv/helix/games/top');
-            console.log(res);
+            let dataArray = res.data.data;
+            let finalArray = dataArray.map(game => {
+                let new_url = game.box_art_url.replace("{width}","250").replace("{height}","300");
+                game.box_art_url = new_url;
+                return game;
+            });
+            setGames(finalArray);
         }
         fetchData();
     },[])
-
     return (
         <div>
             <h1 className="titreGames" >Jeux les plus populaires</h1>
-            <div className="flexAccueil"></div>
+            <div className="flexAccueil">
+                {games.map(
+                    (game,index) => (
+                        <div key={index} className="carteGames">
+                            <img src={game.box_art_url} alt="jeu profile pic" className="imgCarte"/>
+                            <div className="cardBodyGames" >
+                                <h5 className="titreCarteGames" > {game.name}</h5>
+                                <div className="btnCarte" > Regarder {game.name}</div>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
         </div>
     );
     
