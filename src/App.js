80233e12cd7ecc0b38fedad58056d7f20eb0617a
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-03 19:47:36 +0100

    Création de la sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import  React from 'react';
 import Header from "./components/Header/Header";
-
+import SideBar from "./components/SideBar/SideBar";
 function App() {
   return (
     <div className="App">
         <Header/>
+        <SideBar/>
     </div>
   );
 }
---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function SideBar(){
+
+    return (
+        <div className="sideBar">
+            <h1 className="titreSideBar">
+                Chaines recommandees
+            </h1>
+            <ul className="listeStream">
+            </ul>
+        </div>
+    );
+}
+
+export default SideBar;
