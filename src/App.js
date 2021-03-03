ad546513d68241a49c200291f000be3961fcc704
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-03 19:23:59 +0100

    Création du contenu du header

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header></Header>
-      </header>
+        <Header/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import logo from './IconTwitch.svg';
+import search from './Search.svg';
+import menuIco from './Menulco.svg';
 function Header() {
 
     return (
-        <>
-            Header!
-        </>
+        <div>
+          <nav className="headerTop">
+              <ul className="listeMenu" >
+                    <li className="liensNav">
+                        <img src={logo} alt="logo twitch" className={logo}/>
+                    </li>
+                  <li className="liensNav">
+                      Top games
+                  </li>
+                  <li className="liensNav">
+                     Top streams
+                  </li>
+                  <li className="liensNav">
+                       <form className="formSubmit">
+                           <input className="inputRecherche" />
+                           <button type="submit">
+                               <img src={search} alt="icone loupe" className="logoLoupe"/>
+                           </button>
+                       </form>
+                  </li>
+
+              </ul>
+          </nav>
+            <div className="menuResBtn">
+                <img className="menuIco" src={menuIco} alt="icon menu"/>
+            </div>
+        </div>
 
     );
 
