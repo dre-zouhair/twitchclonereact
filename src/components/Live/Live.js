2947eccaf658aed31667e6261bb05a3e9697de20
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-04 00:41:55 +0100

    Adding Video to Live and setting parms and static channel name for testng

--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {ReactTwitchEmbedVideo as Video} from 'react-twitch-embed-video';
 
 function Live() {
 
     return (
         <div>
+            <Video height="754" width="100%" channel="boxbox">
 
+            </Video>
         </div>
     );
 
