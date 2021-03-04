19afc4dd1a90ad2613707143974bcbb14c5b3a32
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-04 13:06:40 +0100

    Mise en place du composant resultat

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TopStreams from "./components/TopStreams/TopStreams";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Live from "./components/Live/Live";
 import GameStreams from "./components/GameStreams/GameStreams";
-
+import Resultats from "./components/Resultats/Resultats";
 function App() {
   return (
       <Router>
@@ -20,6 +20,7 @@ function App() {
                   <Route exact path="/topstreams" component={TopStreams}/>
                   <Route exact path="/live/:slug" component={Live}/>
                   <Route exact path="/game/:slug" component={GameStreams}/>
+                  <Route exact path="/resultats/:slug" component={Resultats}/>
 
               </Switch>
 
