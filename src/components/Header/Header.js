40ddbd0af2d0f1a139ea0fd8fe382ee4fc83159e
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-04 13:24:16 +0100

    rebase + redoing alot of shit tehn commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,50 +1,113 @@
-import React from 'react';
-import logo from './IconTwitch.svg';
-import search from './Search.svg';
-import menuIco from './Menulco.svg';
-import {Link} from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import logo from './IconTwitch.svg'
+import search from './Search.svg'
+import menuIco from './Menulco.svg'
+import croix from './Croix.svg'
+import {Link} from 'react-router-dom';
 
-function Header() {
+function Header(){
+
+    const [menu, showMenu] = useState(false);
+    const [smallScreen, setSmallScreen] = useState(false);
+    const [searchInput, setSearch] = useState('');
+
+    useEffect(() => {
+
+
+        const mediaQuery = window.matchMedia("(max-width: 900px)");
+        // addlistener c'est comme addeventlisterner pour les medias queries en JS
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
+
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        }
+
+    })
+
+    const handleMediaQueryChange = mediaQuery => {
+        if(mediaQuery.matches) {
+            setSmallScreen(true);
+        } else {
+            setSmallScreen(false);
+        }
+    }
+
+    const toggleNavRes = () => {
+        showMenu(!menu);
+    }
+
+    const hideMenu = () => {
+
+        if(menu === true) {
+            showMenu(!menu);
+        }
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleKeyPress = (e) => {
+        setSearch(e.target.value);
+    }
+
 
     return (
         <div>
-          <nav className="headerTop">
-              <ul className="listeMenu" >
-                    <li className="liensNav">
-                        <Link  className="lien" to={'/'}>
-                            <img src={logo} alt="logo twitch" className={logo}/>
-                        </Link>
 
-                    </li>
-                  <li className="liensNav">
-                      <Link className="lien" to={'/'}>
-                          Top games
-                      </Link>
+            <nav className="headerTop">
+                {(menu || !smallScreen) && (
 
-                  </li>
-                  <li className="liensNav">
-                      <Link className="lien" to={'/topstreams'}>
-                          Top Streams
-                      </Link>
-                  </li>
-                  <li className="liensNav">
-                       <form className="formSubmit">
-                           <input className="inputRecherche" />
-                           <button type="submit">
-                               <img src={search} alt="icone loupe" className="logoLoupe"/>
-                           </button>
-                       </form>
-                  </li>
 
-              </ul>
-          </nav>
+                    <ul className="listeMenu">
+
+                        <li onClick={hideMenu} className="liensNav">
+                            <Link className="lien" to="/">
+                                <img src={logo} alt="logo twitch" className="logo"/>
+                            </Link>
+                        </li>
+                        <li onClick={hideMenu} className="liensNav">
+                            <Link className="lien" to="/">
+                                Top Games
+                            </Link>
+                        </li>
+                        <li onClick={hideMenu} className="liensNav">
+                            <Link className="lien" to="/top-streams">
+                                Top Streams
+                            </Link>
+                        </li>
+                        <li className="liensNav">
+                            <form className="formSubmit" onSubmit={handleSubmit}>
+
+                                <input required value={searchInput} onChange={(e) => handleKeyPress(e)} type="text" className="inputRecherche"/>
+
+                                <Link
+                                    className="lien"
+                                    to={{
+                                        pathname: `/resultats/${searchInput}`
+                                    }}
+                                >
+                                    <button type="submit">
+                                        <img src={search} alt="icone loupe" className="logoLoupe"/>
+                                    </button>
+                                </Link>
+                            </form>
+                        </li>
+
+                    </ul>
+
+                )}
+
+            </nav>
+
             <div className="menuResBtn">
-                <img className="menuIco" src={menuIco} alt="icon menu"/>
+                <img onClick={toggleNavRes} src={!menu ? menuIco : croix} alt="icone menu responsive" className="menuIco"/>
             </div>
+
         </div>
-
-    );
-
+    )
 }
 
 export default Header;
