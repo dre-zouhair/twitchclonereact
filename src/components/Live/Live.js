da3caa87dd38a47af0a76b08d7d1f6a2b10b3da1
Drioueche Zouhair <dre.zouhair@gmail.com>
2021-03-04 01:10:51 +0100

    Adding details to Live

--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
 import {useParams} from 'react-router-dom';
+import api from "../../api";
 
 function Live() {
     let {slug} = useParams();
-    return (
-        <div>
-            <ReactTwitchEmbedVideo height="754" width="100%" channel={slug}>
+    const [infoStream,setInfoStream] = useState([]);
+    const [infoGame,setInfoGame] = useState([]);
+    useEffect(
+        () => {
+            const fetchData = async () => {
+                const res = await api.get(`https://api.twitch.tv/helix/streams?user_login=${slug}`);
+                let gameID = res.data.data.map(
+                    game => {
+                        return game.game_id;
+                    }
+                );
+                const resNomGame = await api.get(`https://api.twitch.tv/helix/games?id=${gameID}`);
+                let nomJeu = resNomGame.data.data.map(
+                    gameName => {
+                        return gameName.name;
+                    }
+                );
+                setInfoGame(nomJeu);
+                setInfoStream(res.data.data[0]);
+            }
+            fetchData();
 
+        }
+    ,[]);
+    return (
+        <div className="containerDecale">
+            <ReactTwitchEmbedVideo height="754" width="100%" channel={slug}>
             </ReactTwitchEmbedVideo>
+            <div className="contInfo">
+                <div className="titreStream">
+                    {infoStream.title}
+                </div>
+                <div className="viewer">
+                   Viewrs :  {infoStream.viewer_count}
+                </div>
+                <div className="infogame">
+                    Streamer :  {infoStream.user_name}, &nbsp; langue : {infoStream.language}
+                </div>
+                <div className="nomJeu">
+                    Viewrs :  {infoGame}
+                </div>
+            </div>
         </div>
     );
 
